541efabb988d0c4d0999555a146bb2735e38cd3b
thakur129pc <thakur129pc@gmail.com>
2024-03-31 16:13:04 +0530

    episode-4

--- App.js
+++ App.js
@@ -2,10 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const rootEL = document.getElementById("root");
+
+// React element using React.createElement()
 const container = React.createElement("div", { id: "container" }, [
   React.createElement("h1", { id: "heading-1" }, "Namaste"),
   React.createElement("h2", { id: "heading-2" }, "React"),
   React.createElement("h3", { id: "paragraph" }, "Course"),
 ]);
+// Functional Component
+const FunComp = ({ pass }) => (
+  <div id="comp">Functional Components: {pass}</div>
+);
+// React element using JSX
+const jsxContainer = (
+  <div id="container">
+    <h1 id="heading-1">Namaste</h1>
+    <h2 id="heading-2">React</h2>
+    <h3 id="paragraph">Course</h3>
+    {/* calling functional components */}
+    <FunComp pass={10} />
+  </div>
+);
+
 const root = ReactDOM.createRoot(rootEL);
-root.render(container);
+root.render(jsxContainer);
