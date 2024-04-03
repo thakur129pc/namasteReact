7e830d4732f07cc7d91ca488fc818e9701a4ecc2
thakur129pc <thakur129pc@gmail.com>
2024-04-03 22:15:45 +0530

    till episode 9

--- App.js
+++ App.js
@@ -1,28 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
+// import noData, { data } from "./imp";
+import About from "./About";
+import Yes from "./Yes";
+import { Error } from "./Error";
+import {
+  // BrowserRouter,
+  // Routes,
+  // Route,
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Link,
+} from "react-router-dom";
 
 const rootEL = document.getElementById("root");
 
 // React element using React.createElement()
-const container = React.createElement("div", { id: "container" }, [
-  React.createElement("h1", { id: "heading-1" }, "Namaste"),
-  React.createElement("h2", { id: "heading-2" }, "React"),
-  React.createElement("h3", { id: "paragraph" }, "Course"),
-]);
+// const container = React.createElement("div", { id: "container" }, [
+//   React.createElement("h1", { id: "heading-1" }, "Namaste"),
+//   React.createElement("h2", { id: "heading-2" }, "React"),
+//   React.createElement("h3", { id: "paragraph" }, "Course"),
+// ]);
 // Functional Component
-const FunComp = ({ pass }) => (
-  <div id="comp">Functional Components: {pass}</div>
-);
-// React element using JSX
-const jsxContainer = (
-  <div id="container">
-    <h1 id="heading-1">Namaste</h1>
-    <h2 id="heading-2">React</h2>
-    <h3 id="paragraph">Course</h3>
-    {/* calling functional components */}
-    <FunComp pass={10} />
-  </div>
-);
+const FunComp = ({ pass }) => {
+  return (
+    <div>
+      {pass}
+      <Link to="/about">To about</Link>
+      <Outlet />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <FunComp pass={[10]} />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/yes",
+        element: <Yes />,
+      },
+    ],
+  },
+]);
+
+// const jsxContainer = (
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/main" element={<FunComp pass={[10]} />} />
+//       <Route path="/about" element={<About />} />
+//     </Routes>
+//   </BrowserRouter>
+// );
 
 const root = ReactDOM.createRoot(rootEL);
-root.render(jsxContainer);
+// root.render(jsxContainer);
+root.render(<RouterProvider router={router} />);
